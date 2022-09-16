1294b242f59a96febb2c6becd39234d143bf7ac1
tea <tea.desouza@gmail.com>
2022-09-17 01:12:55 +0800

    Add mappings for CS native ops

--- Assets/Elk/Runtime/Basic/Graph/BinaryOp.cs
+++ Assets/Elk/Runtime/Basic/Graph/BinaryOp.cs
@@ -1,15 +1,18 @@
 using S = System.String;
+using Elk.Bindings.CSharp;
 
 namespace Elk.Basic.Graph{
 public class BinaryOp{
 
     public readonly object arg0, arg1, op;
+    public readonly string binding;
 
-    // TODO probably should be binary exp
+    // TODO probably should be binary exp?
     public BinaryOp(object arg0, object op, object arg1){
         this.arg0 = arg0;
         this.arg1 = arg1;
         this.op   = op;
+        this.binding = CSharpOps.Binary[(string)op];
     }
 
     override public S ToString()
---
+++ Assets/Elk/Runtime/Extern/CSharpOps.cs
@@ -0,0 +1,39 @@
+
+using Map = System.Collections.Generic.Dictionary<string, string>;
+
+// REF https://stackoverflow.com/questions/11113259/
+// how-to-call-custom-operator-with-reflection
+namespace Elk.Bindings.CSharp{
+public static class CSharpOps{
+
+    public static Map Unary = new Map()
+    {
+        {"+", "op_UnaryPlus" },
+        {"-", "op_UnaryMinus" },
+        {"++", "op_Increment" },
+        {"--", "op_Decrement" },
+        {"!", "op_LogicalNot" },
+        {"~", "op_OnesComplement" },
+    };
+
+    public static Map Binary = new Map()
+    {
+        {"+", "op_Addition" },
+        {"-", "op_Subtraction" },
+        {"*", "op_Multiply" },
+        {"/", "op_Division" },
+        {"&", "op_BitwiseAnd" },
+        {"|", "op_BitwiseOr" },
+        {"^", "op_ExclusiveOr" },
+        {"==", "op_Equality" },
+        {"!=", "op_Inequality" },
+        {"<", "op_LessThan" },
+        {">", "op_GreaterThan" },
+        {"<=", "op_LesserThanOrEqual" },
+        {">=", "op_GreaterThanOrEqual" },
+        {"<<", "op_LeftShift" },
+        {">>", "op_RightShift" },
+        {"%", "op_Modulus" },
+    };
+
+}}
