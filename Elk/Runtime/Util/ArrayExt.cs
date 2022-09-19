ec4885098af47814371c9ffe801fe01177beef3b
tea <tea.desouza@gmail.com>
2022-09-20 00:19:15 +0800

    Neaten log output

--- Elk/Runtime/Util/ArrayExt.cs
+++ Elk/Runtime/Util/ArrayExt.cs
@@ -1,20 +1,32 @@
 using System;
+using UnityEngine;
 using System.Text;
 
 namespace Elk.Util{
 public static class ArrayExt{
 
-    public static string Format(this object[] arr){
+    // NOTE: concise but not so clean
+    public static string Format(this object[] arr)
+    => "(" + string.Join(", ", arr) + ")";
+
+    public static string NeatFormat(this object[] arr){
         var o = new StringBuilder();
         o.Append("(");
         var len = arr.Length;
         for(int i = 0; i < len; i++){
-            var elem = arr[i];
-            o.Append(elem != null ? elem : "null");
+            o.Append(ToCleanString(arr[i]));
             if(i < len -1) o.Append(", ");
         }
         o.Append(")");
         return o.ToString();
     }
 
+    static string ToCleanString(object arg){
+        switch(arg){
+            case GameObject go: return go.name;
+            case Transform t:   return t.gameObject.name;
+            case null: return "null";
+            default: return arg.ToString();
+        }
+    }
 }}
---
+++ Elk/Tests/ArrayExtTest.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using NUnit.Framework;
+using Elk.Util;
+
+namespace Elk_Test{
+public class ArrayExtTest{
+
+    [Test] public void TestFormat(){
+        var x = new object[]{ "a", 10, null };
+        Assert.AreEqual("(a, 10, )", x.Format());
+    }
+
+    [Test] public void TestNeatFormat(){
+        var go = new GameObject("Jim");
+        var x = new object[]{ "a", 10, null, go, go.transform };
+        Assert.AreEqual("(a, 10, null, Jim, Jim)", x.NeatFormat());
+        UnityEngine.Object.DestroyImmediate(go);
+    }
+
+}}
