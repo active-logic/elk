7b5ba8ca72bda85ab797134e38a07731ca4fa42e
tea <tea.desouza@gmail.com>
2022-09-29 22:26:42 +0800

    Allow specifying required types

---
+++ BT-lang/Runtime/BTL-Externals.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Ex = System.Exception;
+using Elk.Util;
+using UnityEngine;
+
+namespace Activ.BTL{
+public partial class BTL{
+
+    void EvalExternals(){
+        List<object> ext = new List<object>();
+        foreach(var c in @import) ext.Add(c);
+        foreach(var type in requirements) ext.Add(Req(type));
+        externals = ext.ToArray();
+    }
+
+    object Req(string typeName){
+        var type = TypeFinder.GetType(typeName);
+        if(type == null)throw new Ex(
+            $"Cannot find type [{typeName}]"
+        );
+        var c = GetComponent(type);
+        if(c) return c;
+        c = gameObject.AddComponent(type);
+        if(!c) throw new Ex(
+            $"Cannot add a component of type [{type}]"
+        );
+        return c;
+    }
+
+}}
